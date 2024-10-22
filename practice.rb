9642256d7df8ec263eb35035ead2a64d0a3d8ff1
Chandrakant06 <chandrakant0695@gmail.com>
2024-10-22 19:56:04 +0530

    first commit

---
+++ practice.rb
@@ -0,0 +1,51 @@
+
+
+# hash1 = {"a" => 100, "b" => 200}
+# hash2 = {"c" => 300, "d" => 400}
+
+
+# z = hash1.merge(hash2)
+
+# puts z
+
+# if 4>5
+#   puts "this statement is true"
+# else
+#   puts "this statement is false"
+# end
+
+# x = 4
+# y = 5
+
+# if x>y
+#   puts "false"
+# elsif x>y
+#   puts "true"
+# else
+#   puts "x and y are equal."
+# end
+
+
+
+
+# age = gets.to_i
+# case age
+# when 1..12 then puts "Person is kid."
+# when 13..18 then puts "person is teenager."
+# when 19..45 then "Person is adult."
+# when 26.. 60 then "person is old"
+# end
+
+i = 0
+loop do 
+  i == 2
+  i = i + 2
+  if i == 0
+    next
+  end
+  puts i
+
+  if i == 20
+    break
+  end
+end
